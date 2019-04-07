4dad8ecd5773fbca2c8b96e95358a94a6c8980ac
zhangzhiwei <601460003@qq.com>
2019-04-07 23:15:46 +0800

    提交

---
+++ src/pages/index/car.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="big-box">
+    <v-header title="停车位预约"></v-header>
+  <div style="margin-top: 15px">
+    <div class="box">
+      <label>姓名</label>
+      <input type="text" placeholder="请输入你的真实姓名">
+    </div>
+    <div class="box">
+      <label>车牌号</label>
+      <input type="text" placeholder="粤T-888">
+    </div>
+    <div class="box">
+      <label>车辆类型</label>
+      <input type="text" placeholder="">
+    </div>
+    <div class="box">
+      <label>手机</label>
+      <input type="text" placeholder="输入你的手机号">
+    </div>
+    <div class="box-a">
+      <label>验证码</label>
+      <input type="text" >
+      <input class="btn" type="button" value="获取验证码">
+    </div>
+    <div class="footer-btn"><button class="footer">提交</button></div>
+  </div>
+  </div>
+</template>
+
+<script>
+  import header from '@/components/header'
+  export default {
+    components:{
+      'v-header':header,
+    }
+  }
+</script>
+
+<style scoped>
+  .big-box{
+    height: 100vh;
+    background:#FAFAFA;
+  }
+  .box,.box-a{
+    padding: 15px 20px;
+    font-size: 18px;
+    border-top: 1px solid #FAFAFA;
+    border-bottom: 1px solid #FAFAFA;
+    background: white;
+  }
+  .box input{
+    text-indent: 20px;
+    font-size: 16px;
+  }
+  label{
+    width: 80px;
+    display: inline-block;
+  }
+.box-a .btn{
+  color: #fff;
+  background: #1EB5EF;
+  font-size: 14px;
+  padding: 8px;
+  border-radius: 5px;
+}
+  .footer{
+    color: #fff;
+    background:#1EB5EF ;
+    width: 90%;
+    text-align: center;
+   height: 50px;
+    font-size: 18px;
+    border: none;
+    border-radius: 5px;
+  }
+  .footer-btn{
+    text-align: center;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/pages/index/visit.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="big-box">
+      <v-header title="访客列表"></v-header>
+      <div class="visit-center">
+        <h3>张三 (2人)</h3>
+        <div class="visit-box">
+          <div class="visit-list">
+           <p>手机：18676001200</p>
+            <p>车牌号：粤T-888</p>
+          </div>
+          <div class="visit-list fl">
+            <p>到访：4-4-10</p>
+            <p>业主：张三</p>
+          </div>
+        </div>
+      </div>
+      <div class="visit-center">
+        <h3>李四 (2人)</h3>
+        <div class="visit-box">
+          <div class="visit-list">
+            <p>手机：18676001200</p>
+            <p>车牌号：粤T-888</p>
+          </div>
+          <div class="visit-list fl">
+            <p>到访：4-4-10</p>
+            <p>业主：李四</p>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  import header from '@/components/header'
+  export default {
+    components:{
+      'v-header':header,
+    }
+  }
+</script>
+
+<style scoped>
+  .big-box{
+    height: 100vh;
+    background: #f3f5f7;
+  }
+  .visit-center{
+    margin: 15px 15px;
+    background: white;
+    padding: 20px 15px;
+    border-radius: 8px;
+  }
+  .visit-center h3{
+    font-weight: 700;
+    margin-bottom: 15px;
+  }
+  .visit-box{
+    display: flex;
+  }
+  .visit-list{
+    flex: 1;
+    line-height: 25px;
+    font-size: 15px;
+    color: #999;
+  }
+  .visit-box div:first-child{
+    border-right: 1px solid #999;
+    flex: 1;
+    padding-right: 30px;
+  }
+  .fl{
+    padding-left: 30px;
+  }
+
+</style>
